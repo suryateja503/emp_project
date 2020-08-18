efbe28696b613119503fabde67475cb6862e4563
suryateja503 <aengalasurya121@gmail.com>
2020-08-18 12:46:51 +0530

    initial

---
+++ Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace TCSProject
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        
+            public static void RegisterRoutes(RouteCollection routes)
+            {
+                routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+                routes.MapRoute(
+                    "Default",                                              // Route name
+                    "{controller}/{action}/{id}",                           // URL with parameters
+                    new { controller = "Employee", action = "Login", id = "" }  // Parameter defaults
+                );
+            }
+            protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+        }
+
+        
+    }
+}
